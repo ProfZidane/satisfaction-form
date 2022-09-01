c590e7f838903b87e1ec81e26664d8123a4915dd
ProfZidane <madazada0@gmail.com>
2022-09-01 01:15:10 +0100

    add modif

--- src/app/pages/document/document.component.ts
+++ src/app/pages/document/document.component.ts
@@ -116,6 +116,7 @@ savePDF() {
   setTimeout(() => {
     if (element) {
       element.style.border = "solid 1px gray";
+      alert("Veuillez m'envoyez le document téléchargé par mail : madazada0@gmail.com \nMerci à vous!");
     }    
   })
  
